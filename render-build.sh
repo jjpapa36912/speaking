d0192f385250a692a8526609092969b4ad4db7db
unknown <djk@satreci.com>
2024-11-19 09:08:05 +0900

    portaudio 먼저 설치

--- render-build.sh
+++ render-build.sh
@@ -2,14 +2,17 @@
 
 # pip 업데이트
 python -m pip install --upgrade pip
-pip3 install pipwin
-pip3 install pyaudio
+
 # Update package manager
 apt-get update -y
 
+apt-get install portaudio19-dev
+pip3 install pipwin
+pip3 install pyaudio
+
 # Install PortAudio development libraries
 # apt-get install -y portaudio19-dev
-apt-get install -y portaudio19-dev build-essential libsndfile1 libasound2-dev
+# apt-get install -y portaudio19-dev build-essential libsndfile1 libasound2-dev
 
 
 # Run pip installation
