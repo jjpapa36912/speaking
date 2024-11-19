f588460afa6f8cff35b457459093602ea0dc354d
unknown <djk@satreci.com>
2024-11-19 18:13:10 +0900

    333

--- render-build.sh
+++ render-build.sh
@@ -8,13 +8,23 @@ python -m pip install --upgrade pip
 
 # Update package manager
 apt-get update -y
-apt-get install portaudio19-dev python3-pyaudio
+# apt-get install portaudio19-dev python3-pyaudio
 # apt-get install portaudio19-dev python-all-dev
 # apt-get install portaudio19-dev
 # apt-get install portaudio19-dev  # portaudio 개발 파일 설치
 # apt-get install python3-pyaudio  # PyAudio 설치
 # pip install python3-pyaudio
-pip3 install pyaudio
+# pip3 install pyaudio
+# PortAudio 빌드 및 설치
+cd $HOME
+git clone https://github.com/PortAudio/portaudio.git
+cd portaudio
+./configure --prefix=$HOME/.local
+make
+make install
+
+# PyAudio 설치
+pip install --no-binary :all: --global-option="build_ext" --global-option="-I$HOME/.local/include" --global-option="-L$HOME/.local/lib" pyaudio
 
 
 # sudo apt-get install -y portaudio19-dev
